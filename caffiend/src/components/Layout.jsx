78a88fc80bac5c3264727034917329b9d230c1b3
BethiXSupraja <bethisupraja8@gmail.com>
2025-04-16 17:00:44 -0400

    completed Hero.jsx component

--- caffiend/src/components/Layout.jsx
+++ caffiend/src/components/Layout.jsx
@@ -4,10 +4,23 @@ export default function Layout(props){
 
     const header = (
         <header>
+            <div>
+            <h1 className="text-gradient">CAFFIEND</h1>
+            <p>For Coffe Insatiates</p>
+            </div>
+            <button>
+                <p className="fa-solid fa-mug-hot"> Sign up free</p>
+                <i></i>
+            </button>
+
         </header>
     )
     const footer = (
         <footer>
+            <p><span className="text-gradient">Caffiend</span> was made by <a target="_blank" href="https://react-portfolio-suprajabethi.netlify.app/">Supraja Bethi</a> <br/>using the <a href="https://www.fantacss.smoljames.com" target="_blank">FantaCSS</a> design library by{" "}
+            <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/company/smoljames-education/">
+            @smoljames</a><br />Check out the project on <a target="_blank" href="https://github.com/BethiXSupraja/ReactProjects">GitHub</a>!</p>
+           
         </footer>
     )
    
